75c843d3cffb7866f5bcfe65be731c15f70fbb2b
chu li (微雨亦成诗) <hekailiu@gmail.com>
2025-06-11 13:57:26 +0000

    版权信息 (© [当前年份] 网站名称. All Rights Reserved.)
    备案号 (陕ICP备... 和 陕公网安备...)
    
    链

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -27,6 +27,11 @@ export function Footer() {
             © {currentYear ? currentYear : new Date().getFullYear()} 网站名称. All Rights Reserved.
           </span>
 
+          <div className="flex flex-col sm:flex-row sm:space-x-4">
+            <span>陕ICP备16002024号</span>
+            <span>陕公网安备 61032602000128号</span>
+          </div>
+
           <div className="flex flex-wrap justify-center space-x-4">
             {footerLinks.map((link, index) => (
               <Link key={index} href={link.href} className="hover:text-primary transition-colors">
@@ -34,11 +39,7 @@ export function Footer() {
               </Link>
             ))}
           </div>
-
-          <div className="flex flex-col sm:flex-row sm:space-x-4">
-            <span>陕ICP备16002024号</span>
-            <span>陕公网安备 61032602000128号</span>
-          </div>
+          
           <div className="flex items-center space-x-1.5">
             <span className="inline-block w-2.5 h-2.5 bg-green-500 rounded-full" aria-hidden="true"></span>
             <span>所有业务正常。</span>
